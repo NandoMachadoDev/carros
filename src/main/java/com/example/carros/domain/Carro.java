71836b73bc65d24a7036108e99cea25e0241456f
fernandodevjava <fernando-pgt@outlook.com>
2023-05-23 12:17:07 -0300

    Commit com Banco de Dados

--- src/main/java/com/example/carros/domain/Carro.java
+++ src/main/java/com/example/carros/domain/Carro.java
@@ -1,24 +1,34 @@
 package com.example.carros.domain;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "carro")
 public class Carro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    public Carro() {
+
+    }
 
     public Carro(Long id, String nome) {
         this.id = id;
         this.nome = nome;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
     public void setId(Long id) {
         this.id = id;
     }
     public String getNome() {
         return nome;
     }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    public void setNome(String nome) { this.nome = nome; }
 }
---
+++ src/main/java/com/example/carros/domain/CarrosRepository.java
@@ -0,0 +1,6 @@
+package com.example.carros.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarrosRepository extends CrudRepository<Carro, Long> {
+}
